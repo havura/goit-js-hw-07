76d941676f23a95845424834064a7b0446da5df9
Havura Artur <havuraa@gmail.com>
2023-02-14 17:22:18 +0200

    adding first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galerryColection = document.querySelector('.gallery');
+
+const createGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview} "
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join('');
+
+const clickOnGalleryColection = (event) => {
+    event.preventDefault();
+    
+    const { target } = event;
+
+    if (target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src= "${target.dataset.source}">
+`);
+    instance.show();
+    
+};
+
+galerryColection.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+galerryColection.addEventListener('click', clickOnGalleryColection);
