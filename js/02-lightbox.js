8e52f9cba38247f18c5aa5d01c17efe62454fa42
Havura Artur <havuraa@gmail.com>
2023-02-14 19:00:47 +0200

    adding second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galerryColection = document.querySelector('.gallery');
+
+const createGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview} "
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+    .join('');
+
+galerryColection.insertAdjacentHTML('afterbegin', createGalleryItems);
+
+new SimpleLightbox('.gallery a', {captionsData:'alt', captionDelay: 250, });
